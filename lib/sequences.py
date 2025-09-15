53879bb1113427488a0d58e93a346f19e6ec9be1
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-09-15 09:55:42 +0300

    solved

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib =[]
+    for i in range(length):
+        if i == 0:
+            fib.append(0)
+        elif i == 1:
+            fib.append(1)
+        else:
+            fib.append(fib[i-1] + fib[i-2])
+    print(fib)
+        
+    
